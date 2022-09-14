8d0cc13b4d39d88708832109b872c8f503c860aa
madhead <siarhei.krukau@gmail.com>
2022-09-14 21:29:50 +0200

    Sync madhead/docker-oracle-xe

--- madhead-docker.tf
+++ madhead-docker.tf
@@ -8,11 +8,26 @@ resource "gitlab_group" "madhead-docker" {
   auto_devops_enabled    = false
 }
 
-resource "gitlab_project" "example" {
+resource "gitlab_project" "docker-image" {
   name                       = "docker-image"
   namespace_id               = gitlab_group.madhead-docker.id
   visibility_level           = "public"
   import_url                 = "https://github.com/madhead/docker-image.git"
+  mirror                     = true
+  issues_enabled             = false
+  merge_requests_enabled     = false
+  pipelines_enabled          = false
+  wiki_enabled               = false
+  snippets_enabled           = false
+  container_registry_enabled = false
+  packages_enabled           = false
+}
+
+resource "gitlab_project" "docker-oracle-xe" {
+  name                       = "docker-oracle-xe"
+  namespace_id               = gitlab_group.madhead-docker.id
+  visibility_level           = "public"
+  import_url                 = "https://github.com/madhead/docker-oracle-xe.git"
   mirror                     = true
   issues_enabled             = false
   merge_requests_enabled     = false
